09b57fbb774931de8e6f362fbac118785e6027fe
Alan Thomas <alan.thomas@minted.com>
2016-02-23 20:50:47 -0600

    add convenience imports and markdown parser

--- playground/dom-rendering/main.js
+++ playground/dom-rendering/main.js
@@ -1,7 +1,21 @@
 'use strict';
 
 var request = require('superagent');
+var markdown = require('markdown').markdown;
 
+/* Convenience imports  */
+
+// React
+// var React = require('react');
+// var ReactDOM = require('react-dom');
+
+// AngularJS
+// var angular = require('angular');
+
+// jQuery
+// var $ = require('jquery');
+
+// Example xhr
 request
   .get('/api/items')
   .end(function(err, res) {
