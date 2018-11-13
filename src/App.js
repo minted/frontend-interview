753ed26e2d9acdba1d784bc3f686ee9fa6c5c06f
Alan Christopher Thomas <alan.thomas@minted.com>
2018-11-13 11:13:14 -0600

    Add UI for jest tests

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ class App extends Component {
     return (
       <div className="container">
         <h1>Interview Playgrounds</h1>
+        <ul>
+          <li>
+            <strong>Coding:</strong> <a href="/" onClick={e => e.target.port = 3005}>Fruit basket</a> (test runner)
+          </li>
+        </ul>
       </div>
     );
   }
