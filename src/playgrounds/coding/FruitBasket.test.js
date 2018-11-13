7a70bfe183fac52dde4e3de06a6b101149f81104
Alan Christopher Thomas <alan.thomas@minted.com>
2018-11-13 09:01:28 -0600

    Add more fruit basket tests for some refactoring

--- src/playgrounds/coding/FruitBasket.test.js
+++ src/playgrounds/coding/FruitBasket.test.js
@@ -4,14 +4,14 @@ import FruitBasket from './FruitBasket';
 
 it('gets the fruits', () => {  // already passes!
   const widget = new FruitBasket(['apple', 'banana']);
-  const response = widget.getFruits();
-  expect(response).toEqual(['apple', 'banana']);
+  const fruits = widget.getFruits();
+  expect(fruits).toEqual(['apple', 'banana']);
 });
 
 it('gets the total number of fruits', () => {
   const widget = new FruitBasket(['apple', 'banana']);
-  const response = widget.getFruitCount();
-  expect(response).toEqual(2);
+  const fruitCount = widget.getFruitCount();
+  expect(fruitCount).toEqual(2);
 });
 
 it('adds a fruit', () => {
@@ -29,6 +29,45 @@ it('checks for a fruit', () => {
 
 it('reports the number of each fruit', () => {
   const widget = new FruitBasket(['apple', 'banana', 'banana', 'lime']);
+  const report = widget.getFruitReport();
+  expect(report).toEqual({
+    'apple': 1,
+    'banana': 2,
+    'lime': 1,
+  });
+});
+
+it('understands fruit characteristics', () => {
+  const widget = new FruitBasket([
+    { name: 'apple', isCitrus: true },
+    { name: 'banana', isCitrus: false },
+    { name: 'lime', isCitrus: true },
+  ]);
+
+  const report = widget.getCitrusFruits();
+  expect(report).toEqual(['apple', 'lime']);
+});
+
+it('fully handles fruit objects with characteristics', () => {
+  const widget = new FruitBasket([
+    { name: 'apple', isCitrus: true },
+    { name: 'banana', isCitrus: false },
+  ]);
+
+  let fruits = widget.getFruits();
+  expect(fruits).toEqual(['apple', 'banana']);
+
+  const fruitCount = widget.getFruitCount();
+  expect(fruitCount).toEqual(2);
+
+  widget.addFruit({ name: 'lime', isCitrus: true });
+  fruits = widget.getFruits();
+  expect(fruits).toEqual(['apple', 'banana', 'lime']);
+
+  expect(widget.hasFruit('banana')).toBe(true);
+  expect(widget.hasFruit('orange')).toBe(false);
+
+  widget.addFruit({ name: 'banana', isCitrus: false });
   const report = widget.getFruitReport();
   expect(report).toEqual({
     'apple': 1,
