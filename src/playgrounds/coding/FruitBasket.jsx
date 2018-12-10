ab52abcf5d19f07dd8b66d78185cd0e1abd47bc3
Alan Christopher Thomas <alan.thomas@minted.com>
2018-12-10 12:30:59 -0600

    Just use jest tests in cli for now (majestic only supports localhost)

--- src/playgrounds/coding/FruitBasket.jsx
+++ src/playgrounds/coding/FruitBasket.jsx
@@ -18,9 +18,6 @@ class FruitBasket extends Component {
       <div>
         {/* TODO 1. nothing to do here :) */}
         <h1>Fruit Basket</h1>
-        <p>
-          <strong>Note:</strong> Open the tests UI in your browser (preview URL in tests tab in Codenvy) for the best experience.
-        </p>
         <ul>
           {/* TODO 2. make this render the list of fruits */}
           {/* TODO 5. make special fruits from props bold */}
